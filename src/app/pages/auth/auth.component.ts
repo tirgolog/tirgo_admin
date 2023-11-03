e893d46492792491a2ced35bc6c5aab3f0fbe179
tirgo <u.rahimov@tirgo.uz>
2023-11-03 12:06:17 +0500

    first commit

---
+++ src/app/pages/auth/auth.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { ToastrService } from 'ngx-toastr';
+import { SpollersService } from 'src/app/services/spollers.service';
+import {Router} from "@angular/router";
+
+@Component({
+   selector: 'app-auth',
+   templateUrl: './auth.component.html',
+   styleUrls: ['./auth.component.scss'],
+})
+
+
+
+
+export class AuthComponent {
+
+   login: string = "admin"
+   password: string = "admin"
+   error: boolean = false
+
+   constructor(
+      private authService: AuthService,
+      private app: AppComponent,
+      private router: Router,
+      private toastr: ToastrService,
+   ) {
+
+   }
+
+   ngOnInit() {
+      if (this.authService.isAuthenticated()) {
+         //this.router.navigate(['dashboard']);
+      }
+   }
+   async getLogin() {
+      const res = await this.authService.loginAdmin(this.login, this.password).toPromise();
+      if (res.status) {
+         await this.authService.setJwt(res.token);
+         this.error = false
+      } else {
+         this.error = true
+         this.toastr.error(res.error)
+      }
+   }
+}
---
+++ src/app/pages/user/user.component.ts
@@ -0,0 +1,68 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {AuthService} from "../../services/auth.service";
+import {ToastrService} from "ngx-toastr";
+import {HelperService} from "../../services/helper.service";
+import {AddTransactionsComponent} from "../../components/add-transactions/add-transactions.component";
+
+@Component({
+   selector: 'app-user',
+   templateUrl: './user.component.html',
+   styleUrls: ['./user.component.scss'],
+   host: { "id": "main" }
+})
+export class UserComponent {
+   user: any;
+   constructor(
+       @Inject(MAT_DIALOG_DATA) public data: any,
+       public dialog: MatDialog,
+       private authService: AuthService,
+       private toastr: ToastrService,
+       public helper: HelperService,
+   ) {
+   }
+   async ngOnInit(){
+      console.log(this.data)
+      const res = await this.authService.getUserInfo(+this.data).toPromise();
+      console.log(res)
+      if (res.status) {
+         this.user = res.data
+      }
+   }
+   addMoney() {
+      this.dialog.open(AddTransactionsComponent, {
+         data:this.data
+      });
+   }
+   async generPasswordMerchant(){
+      const res = await this.authService.generPasswordMerchant(+this.data,this.user.name).toPromise();
+      console.log(res)
+      if (res.status) {
+         this.user.merch_login = res.name;
+         this.user.merch_password = res.code;
+      }
+   }
+   async saveNewMerchantId(){
+      const confirm = await this.helper.openDialogConfirm('Вы уверены?', 'Вы уверены что хотите добавить водителя вк данному мерчанту?', 2)
+      if (confirm){
+         const res = await this.authService.saveNewMerchantId(this.user.merch_id,this.user.id).toPromise();
+         if (res.status) {
+            this.toastr.success("Информация о пользователе изменена");
+         } else {
+            this.toastr.error(res.error)
+         }
+      }
+   }
+   goToColumn(id) {
+      if (id){
+         const dialogRef = this.dialog.open(UserComponent, {
+            width: '90%',
+            height: '80%',
+            panelClass: 'custom-dialog-class',
+            data: id
+         });
+      }else {
+         this.toastr.error('Сначала нужно назначить мерчанта')
+      }
+   }
+}
