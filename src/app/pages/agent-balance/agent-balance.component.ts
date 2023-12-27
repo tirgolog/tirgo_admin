9d6e83ef0fe2096d6b1d203e54829128acd49ff6
nemo.falcon96 <nemo.falcon96@gmail.com>
2023-12-27 17:50:01 +0500

    agent list and change balance

---
+++ src/app/pages/agent-balance/agent-balance.component.ts
@@ -0,0 +1,52 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { HelperService } from 'src/app/services/helper.service';
+@Component({
+  selector: 'app-agent-balance',
+  templateUrl: './agent-balance.component.html',
+  styleUrls: ['./agent-balance.component.scss']
+})
+export class AgentBalanceComponent {
+  getFile(e: any, el: any) {
+     el.value = e.files[0].name
+  }
+  data = {
+     agent_id: 0,
+     agent_balance: '',
+  }
+  constructor(
+     @Inject(MAT_DIALOG_DATA) public dataDialog: any,
+     public dialog: MatDialog,
+     private authService: AuthService,
+     private toastr: ToastrService,
+     public helper: HelperService) {
+     if (this.dataDialog) {
+      console.log(this.dataDialog)
+        this.data.agent_id = this.dataDialog.agent_id;
+        this.data.agent_balance = this.dataDialog.agent_balance;
+    
+     }
+  }
+  close() {
+     this.dialog.closeAll()
+  }
+  async editBalance() {
+     if (!this.data.agent_balance || !this.data.agent_id ) {
+        await this.helper.openDialogConfirm('Ошибка', 'Не все поля заполнены корректно', 1)
+     } else {
+        const confirm = await this.helper.openDialogConfirm('Вы уверены?', 'Вы уверены что хотите добавить данный тип подписка?', 2)
+        if (confirm) {
+              const res = await this.authService.EditAgentBalance(this.data).toPromise();
+              if (res.status) {
+                 this.toastr.success('Баланс агента успешно редактировать')
+                 this.dialog.closeAll();
+              } else {
+                 this.toastr.error(res.error)
+              }
+
+        }
+     }
+  }
+}
--- src/app/pages/agent-list/agent-list.component.ts
+++ src/app/pages/agent-list/agent-list.component.ts
@@ -4,16 +4,15 @@ import { SpollersService } from 'src/app/services/spollers.service';
 import { AuthService } from "../../services/auth.service";
 import { HelperService } from "../../services/helper.service";
 import { ListService } from "../../services/list.service";
-import { DriverVerifyComponent } from '../driver-verify/driver-verify.component';
+
 @Component({
-  selector: 'app-driver-verifed',
-  templateUrl: './driver-verifed.component.html',
-  styleUrls: ['./driver-verifed.component.scss'],
+  selector: 'app-agent-list',
+  templateUrl: './agent-list.component.html',
+  styleUrls: ['./agent-list.component.scss'],
   host: { "id": "main" }
 })
-export class DriverVerifedComponent {
-
-  drivers: any[] = [];
+export class AgentListComponent {
+  agents: any[] = [];
   count: any;
   loading: boolean = false;
   sizespage = [
@@ -33,27 +32,18 @@ export class DriverVerifedComponent {
 
   async handlePage(e: any) {
     this.loading = true;
-    let drivers = await this.listService.getAllVerifiedDrivers().toPromise();
-    this.drivers = drivers.data;
+    let drivers = await this.listService.getAllAgent().toPromise();
+    this.agents = drivers.data;
     this.count = drivers.data_count;
     this.loading = false;
   }
 
   async filterList() {
     this.loading  = true;
-    let drivers = await this.listService.getAllVerifiedDrivers().toPromise();
-    this.drivers = drivers.data;
+    let drivers = await this.listService.getAllAgent().toPromise();
+    this.agents = drivers.data;
     this.count = drivers.data_count;
     this.loading = false;
-  }
-
-  goToColumn(ev: any): void {
-    const dialogRef = this.dialog.open(DriverVerifyComponent, {
-      width: '60%',
-      height: '70%',
-      panelClass: 'custom-dialog-class',
-      data: ev
-    });
   }
 
 }
